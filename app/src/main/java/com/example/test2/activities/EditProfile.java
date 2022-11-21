86761797672cc7f7785766dc2261704250318e67
Pawan <pavanpanchal1998@gmail.com>
2022-11-21 18:03:17 -0500

    Settings with edit profile and logout functionality

---
+++ app/src/main/java/com/example/test2/activities/EditProfile.java
@@ -0,0 +1,147 @@
+package com.example.test2.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.test2.R;
+import com.example.test2.classes.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class EditProfile extends AppCompatActivity implements View.OnClickListener {
+
+    private DatabaseReference reference;
+    private EditText editTextFullName, editTextEmailID, editTextAge, editTextPhoneNumber, editTextLicenseNo,editTextAddress, editTextPassword;
+    private  String fullName,email, age, phone_number, license_no, address;
+    private String UserID;
+    private FirebaseDatabase rootNode;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+        Button submit = findViewById(R.id.submit);
+        submit.setOnClickListener(this);
+        editTextFullName = (EditText) findViewById(R.id.full_name);
+        editTextEmailID= (EditText) findViewById(R.id.username);
+        editTextAge= (EditText) findViewById(R.id.age);
+        editTextPhoneNumber= (EditText) findViewById(R.id.phone_number);
+        editTextLicenseNo= (EditText) findViewById(R.id.license_no);
+        editTextAddress = (EditText) findViewById(R.id.address);
+        editTextPassword= (EditText) findViewById(R.id.password);
+
+        UserID = (FirebaseAuth.getInstance().getCurrentUser().getUid());
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(UserID);
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                 fullName = userProfile.getFullName();
+                 email = userProfile.getEmail();
+                 age = userProfile.getAge();
+                 phone_number = userProfile.getPhoneNumber();
+                 license_no = userProfile.getLicenseNo();
+                 address = userProfile.getAddress();
+
+                TextView FullNameTextView = (TextView) findViewById(R.id.full_name);
+                TextView emailTextView  = (TextView) findViewById(R.id.username);
+                TextView AgeTextView  = (TextView) findViewById(R.id.age);
+                TextView PhoneNumberTextView  = (TextView) findViewById(R.id.phone_number);
+                TextView LicenseNoTextView  = (TextView) findViewById(R.id.license_no);
+                TextView AddressTextView  = (TextView) findViewById(R.id.address);
+                TextView PasswordTextView  = (TextView) findViewById(R.id.password);
+
+
+
+
+
+                FullNameTextView.setHint(fullName);
+                emailTextView.setHint(email);
+                AgeTextView.setHint(age);
+                PhoneNumberTextView.setHint(phone_number);
+                LicenseNoTextView.setHint(license_no);
+                AddressTextView.setHint(address);
+                PasswordTextView.setHint("***********");
+            }
+
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(EditProfile.this,"Something went wrong!", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.submit:
+                 UpdateProfile();
+                 break;
+        }
+    }
+
+    private void UpdateProfile(){
+        String email = editTextEmailID.getText().toString().trim();
+        String FullName = editTextFullName.getText().toString().trim();
+        String Age= editTextAge.getText().toString().trim();
+        String PhoneNumber= editTextPhoneNumber.getText().toString().trim();
+        String LicenseNo= editTextLicenseNo.getText().toString().trim();
+        String Address= editTextAddress.getText().toString().trim();
+        String Password= editTextPassword.getText().toString().trim();
+
+
+        reference = FirebaseDatabase.getInstance().getReference().child("Users").child(UserID);
+        if (FullName != null) {
+            reference.child("fullName").setValue(FullName);
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmailID.setError("Please provide valid email id !");
+            editTextEmailID.requestFocus();
+            return;
+        }
+        else if(email != null){
+
+            reference.child("email").setValue(email);
+        }
+        if (Age != null) {
+            reference.child("age").setValue(age);
+        }
+        if (PhoneNumber != null) {
+            reference.child("phoneNumber").setValue(PhoneNumber);
+        }
+        if (LicenseNo != null) {
+            reference.child("licenseNo").setValue(LicenseNo);
+        }
+        if (Address != null) {
+            reference.child("address").setValue(Address);
+        }
+        if(Password != null){
+            reference.child("password").setValue(Password);
+        }
+
+    }
+
+
+}
--- app/src/main/java/com/example/test2/activities/UserSettings.java
+++ app/src/main/java/com/example/test2/activities/UserSettings.java
@@ -1,19 +1,31 @@
 package com.example.test2.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.test2.classes.User;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import com.example.test2.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
 public class UserSettings extends AppCompatActivity implements View.OnClickListener {
     private FirebaseAuth Auth;
+    private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +36,7 @@ public class UserSettings extends AppCompatActivity implements View.OnClickListe
         Button SignOut =  findViewById(R.id.buttonLogout);
         SignOut.setOnClickListener(this);
 
-        Button EditProfile = findViewById(R.id.EditProfile);
+        Button EditProfile = findViewById(R.id.edit_profile);
         EditProfile.setOnClickListener(this);
 
     }
@@ -36,17 +48,63 @@ public class UserSettings extends AppCompatActivity implements View.OnClickListe
                 Auth.signOut();  // method with the name of RegisterUser
                 startActivity(new Intent(this, MainActivity.class));
                 break;
-            case R.id.EditProfile:
-                edit_user_profile();
-
-
+            case R.id.edit_profile:
+                startActivity(new Intent(this, EditProfile.class));
 
         }
 }
 
     private void edit_user_profile(){
         setContentView(R.layout.activity_edit_profile);
-        //UserID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+
+        String UserID = (FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(UserID);
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User userProfile = snapshot.getValue(User.class);
+
+                String fullName = userProfile.getFullName();
+                String email = userProfile.getEmail();
+                String age = userProfile.getAge();
+                String phone_number = userProfile.getPhoneNumber();
+                String license_no = userProfile.getLicenseNo();
+                String address = userProfile.getAddress();
+
+                TextView FullNameTextView = (TextView) findViewById(R.id.full_name);
+                TextView emailTextView  = (TextView) findViewById(R.id.username);
+                TextView AgeTextView  = (TextView) findViewById(R.id.age);
+                TextView PhoneNumberTextView  = (TextView) findViewById(R.id.phone_number);
+                TextView LicenseNoTextView  = (TextView) findViewById(R.id.license_no);
+                TextView AddressTextView  = (TextView) findViewById(R.id.address);
+                TextView PasswordTextView  = (TextView) findViewById(R.id.password);
+                Button submit = findViewById(R.id.submit);
+//                submit.setOnClickListener(this);
+
+
+                FullNameTextView.setHint(fullName);
+                emailTextView.setHint(email);
+                AgeTextView.setHint(age);
+                PhoneNumberTextView.setHint(phone_number);
+                LicenseNoTextView.setHint(license_no);
+                AddressTextView.setHint(address);
+                PasswordTextView.setHint("Password");
+            }
+
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(UserSettings.this,"Something went wrong!", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+
+
 
 
 
